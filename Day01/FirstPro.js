afd1ebb7697b5ae742ffdfc34f415d2cac2d8078
DilkiWijewardhana <isharawijewardhana5@gmail.com>
2025-05-29 01:01:04 -0700

    Add files via upload

---
+++ Day01/FirstPro.js
@@ -0,0 +1,40 @@
+console.log("Hello!")
+
+let num1 = 10
+let num2 = 5
+var x = 2
+var y = 3
+const z = 10
+
+console.log(num1 + num2)
+
+console.log(x + y)
+
+console.log(z)
+
+//print numbers from 1 to 10 
+for(x = 1; x <= 10; x++)
+{
+	console.log(x)
+}
+
+
+//print only the odd numbers
+for(var x = 1; x <= 10; x++)
+{
+	if(x % 2)
+	{
+		console.log(x)
+	}
+}
+
+
+//print the numbers in reverse
+for(x = 10; x >= 1; x--)
+{
+	console.log(x)
+}
+
+
+//reverse the numbers in left and right 4321-5-9876
+console.log(parseInt(9/2))
